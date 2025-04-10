91c584dca888c6a509a7913844bd90b642a92311
Tymur <tn3996580@gmail.com>
2025-04-11 01:26:08 +0300

    correct hw5, problem was in using different libs

--- hw5/hw_5_1.js
+++ hw5/hw_5_1.js
@@ -1,31 +1,37 @@
 const fs = require("fs");
+const crypto = require("crypto");
 const { createHmac } = require("node:crypto");
 const bcrypt = require("bcrypt");
 
 const secret = "homa";
 
-const pass1 = 12345;
-const pass2 = 54321;
-const pass3 = 10010;
-const pass4 = 88888;
-const pass5 = 54321;
+const pass1 = "12345";
+const pass2 = "54321";
+const pass3 = "10010";
+const pass4 = "88888";
+const pass5 = "54321";
 
 const arrOfPass = [pass1, pass2, pass3, pass4, pass5];
 const arrOfHash = [];
 
 arrOfPass.forEach((pass) => {
-  const hash = createHmac("sha256", secret).update(`${pass}`).digest("hex");
+  const hash = crypto
+    .createHash("sha256", secret)
+    .update(`${pass}`)
+    .digest("hex");
   arrOfHash.push(hash);
 });
 
-for (let i = 0; i < arrOfPass.length; i++) {
-  bcrypt.compare(arrOfPass[i], arrOfHash[i], (err, result) => {
-    if (result) {
-      console.log("✅ right");
-    } else {
-      console.log("❌ left");
-    }
-  });
+for (let i = 0; i < arrOfHash.length; i++) {
+  const newHash = crypto
+    .createHash("sha256", secret)
+    .update(`${arrOfPass[i]}`)
+    .digest("hex");
+  if (newHash === arrOfHash[i]) {
+    console.log("✅ success");
+  } else {
+    console.log("❌ failed");
+  }
 }
 
-console.log(arrOfHash);
+// console.log(arrOfHash);

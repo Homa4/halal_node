6f27e7ecb42f705559ce8079fb2fd2bce1be286b
Tymur <tn3996580@gmail.com>
2025-04-10 16:28:36 +0300

    add second variation and rewrite previous

--- hw4/hw_4_2.js
+++ hw4/hw_4_2.js
@@ -3,15 +3,18 @@ const path = require("path");
 
 const pathToFoder = "./text";
 const files = fs.readdirSync(pathToFoder);
+
 let largestSize = 0;
 
 files.forEach((file) => {
-  fs.stat(path.join("./text/" + file))
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => console.log(error));
-  console.log(data);
+  const filePath = path.join(pathToFoder, file);
+
+  const stat = fs.statSync(filePath);
+  if (stat.isFile()) {
+    if (stat.size > largestSize) {
+      largestSize = stat.size;
+    }
+  }
 });
 
 if (largestSize) {

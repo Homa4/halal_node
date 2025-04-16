2a9e0e2657b83896d86fa82ff6f5cb90e7a32f29
Tymur <tn3996580@gmail.com>
2025-04-17 00:38:08 +0300

    modify hw13

--- hw13/hw_13_1.js
+++ hw13/hw_13_1.js
@@ -1,19 +1,20 @@
 const express = require("express");
-
 const app = express();
 const PORT = 3000;
 
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+const users = [
+  { name: "Anna", age: 25, email: "anna@example.com" },
+  { name: "Inna", age: 30, email: "inna@example.com" },
+  { name: "Alex", age: 22, email: "alex@example.com" },
+];
+
 app.get("/", (req, res) => {
-  try {
-    // res.writeHead(200, { "content-type": "aplication/html" });
-    app.set("view engine", "pug");
-    app.set("views", "./views");
-    res.render("index", { title: "Shalom", message: "message" });
-  } catch (err) {
-    console.log(err);
-  }
+  res.render("index", { users });
 });
 
 app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
+  console.log(`Сервер запущено на http://localhost:${PORT}`);
 });
